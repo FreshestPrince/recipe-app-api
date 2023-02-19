fe06d10f273bc103d84fdd377e42babc1fede03a
hugburke <hugburke97@gmail.com>
2023-02-19 11:54:42 +0000

    .

--- app/user/views.py
+++ app/user/views.py
@@ -1,33 +1,32 @@
 """
-Views for the user API.
+Views for the recipe APIs
 """
-from rest_framework import generics, authentication, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-from user.serializers import (
-    UserSerializer,
-    AuthTokenSerializer,
-)
+from core.models import Recipe
+from recipe import serializers
 
 
-class CreateUserView(generics.CreateAPIView):
-    """Create a new user in the system."""
-    serializer_class = UserSerializer
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
 
-class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user."""
-    serializer_class = AuthTokenSerializer
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
 
+        return self.serializer_class
 
-class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manage the authenticated user."""
-    serializer_class = UserSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        """Retrieve and return the authenticated user."""
-        return self.request.user
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
